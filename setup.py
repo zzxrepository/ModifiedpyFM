998e406476c3fd86d5f3d1968ba0e522c1b65982
zzx <zzxkingdom@163.com>
2024-01-21 16:29:50 +0800

    zzx

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages, Extension
+from Cython.Distutils import build_ext
+import numpy
+
+setup(
+	maintainer='Corey Lynch',
+    name='pyfm',
+    packages=find_packages(),
+    url='https://github.com/coreylynch/pyFM',
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("pyfm_fast", ["pyfm_fast.pyx"],
+    						 include_dirs=[numpy.get_include()])]
+)
